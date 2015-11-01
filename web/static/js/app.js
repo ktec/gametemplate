f3173c36232f0fb45bbaf026a114500a0b7006b4
Keith Salisbury <keith.salisbury@alliants.com>
2015-11-01 22:25:50 +0000

    logo fun

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -28,6 +28,7 @@ class PlayState extends Phaser.State {
   }
 
   create() {
+    this.game.physics.startSystem(Phaser.Physics.ARCADE)
     this.logo = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'phaser')
     this.logo.anchor.setTo(0.5, 0.5)
     this.up_key = this.game.input.keyboard.addKey(Phaser.Keyboard.DOWN)
@@ -35,20 +36,7 @@ class PlayState extends Phaser.State {
     this.left_key = this.game.input.keyboard.addKey(Phaser.Keyboard.LEFT)
     this.right_key = this.game.input.keyboard.addKey(Phaser.Keyboard.RIGHT)
     this.space_key = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR)
-    this.emitter = this.game.add.emitter(this.game.world.centerX, 200, 200)
-    // emitter.width = 800
-    this.emitter.makeParticles('elephant')
-    this.emitter.minParticleSpeed.set(-400, -300)
-    this.emitter.maxParticleSpeed.set(400, 300)
-    // this.emitter.minParticleSpeed.setTo(-300, -300)
-    // this.emitter.maxParticleSpeed.setTo(300, 300)
-
-    this.emitter.setAlpha(0.3, 0.8)
-    this.emitter.setScale(0.1, 0.1, 0.2, 0.2)
-    // this.emitter.gravity = -200
-    this.emitter.minRotation = 0
-    this.emitter.maxRotation = 0
-    this.emitter.start(false, 1000, 100)
+    this.emitter = this.createEmitter(this.logo, this.game)
   }
 
   zoomOut() {
@@ -63,8 +51,6 @@ class PlayState extends Phaser.State {
     this.logo.scale.setTo(x * 1.05, y * 1.05)
   }
 
-
-
   rotateLeft() {
     this.logo.rotation -= 0.1
   }
@@ -74,13 +60,40 @@ class PlayState extends Phaser.State {
     this.emitter.setRotation(this.logo.rotation, this.logo.rotation)
   }
 
+  createEmitter(sprite, game) {
+
+    let emitter = game.add.emitter(sprite.x - 40 , sprite.y - 140, 1000)
+    emitter.makeParticles('elephant')
+    emitter.minParticleSpeed.set(-400, 400)
+    emitter.maxParticleSpeed.set(400, 400)
+    emitter.setAlpha(0.3, 0.8)
+
+    // setScale(minX, maxX, minY, maxY, rate, ease, yoyo)
+    emitter.setScale(0.01, 0.1, 0.01, 0.1, 0, Phaser.Easing.Quadratic.Out, true)
+    
+    // emitter.gravity = -500
+    // emitter.minRotation = 0
+    // emitter.maxRotation = 0.5
+    // Attach the emitter to the sprite
+    // emitter.minParticleSpeed.setTo(-300, -300)
+    // emitter.maxParticleSpeed.setTo(200, 400)
+    // emitter.minParticleScale = 0.1
+    // emitter.maxParticleScale = 0.5
+    // emitter.bounce.setTo(0.5, 0.5)
+    emitter.angularDrag = 30;
+
+    //sprite.addChild(emitter)
+    // emitter.start(false, 1000, 100)
+    return emitter
+  }
+
 	update() {
+    this.game.physics.arcade.collide(this.emitter)
     if (this.down_key.isDown) { this.zoomIn() }
     if (this.up_key.isDown) { this.zoomOut() }
     if (this.left_key.isDown) { this.rotateLeft() }
     if (this.right_key.isDown) { this.rotateRight() }
-    if (this.space_key.isDown) { this.emitter.on = true }
-    else { this.emitter.on = false }
+    if (this.space_key.isDown) { this.emitter.emitParticle() }
 	}
 }
 
