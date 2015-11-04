5fcde957420ce2e4d79d22bbf74badb31e68261f
Keith Salisbury <keith.salisbury@alliants.com>
2015-11-04 00:06:39 +0000

    added simple phaser emitter

--- web/static/js/states/menu.js
+++ web/static/js/states/menu.js
@@ -1,21 +1,21 @@
 export class MenuState extends Phaser.State {
   create() {
-    var style = { font: '65px Arial', fill: '#ffffff', align: 'center'};
+    let style1 = { font: '65px Arial', fill: '#ffffff', align: 'center'}
+    let style2 = { font: '16px Arial', fill: '#ffffff', align: 'center'}
 
-    this.titleText = this.game.add.text(this.game.world.centerX, 300, 'Tic-Tac-Toe', style);
-    this.titleText.anchor.setTo(0.5, 1);
+    this.titleText = this.addCenteredText('Elephant Tickle', 300, style1)
+    this.instructionsText = this.addCenteredText('Tickle the elephants', 400, style2)
+  }
 
-    this.instructionsText = this.game.add.text(this.game.world.centerX, 400, '1 Player', { font: '16px Arial', fill: '#ffffff', align: 'center'});
-    this.instructionsText.anchor.setTo(0.5, 0.5);
-
-    this.instructionsText = this.game.add.text(this.game.world.centerX, 400, '2 Player', { font: '16px Arial', fill: '#ffffff', align: 'center'});
-    this.instructionsText.anchor.setTo(0.5, -2);
-    console.log(this.game.input.currentPointers);
+  addCenteredText(str, yPos, style) {
+    let text = this.game.add.text(this.game.world.centerX, yPos, str, style)
+    text.anchor.setTo(0.5, 1)
+    return text
   }
 
 	update() {
     if(this.game.input.activePointer.justPressed()) {
-      this.game.state.start('play');
+      this.game.state.start('play')
     }
 	}
 }
--- web/static/js/states/play.js
+++ web/static/js/states/play.js
@@ -1,48 +1,78 @@
 export class PlayState extends Phaser.State {
   create() {
-    var CELL_WIDTH, CELL_HEIGHT,
-        CELL_COLS, CELL_ROWS
-
-        CELL_WIDTH = CELL_HEIGHT = 107
-        CELL_COLS = CELL_ROWS = 3
-
-    this.cells = this.game.add.group()
-    this.player = 1
-
-    this.cells.physicsBodyType = Phaser.Physics.ARCADE
-
-    for (var i = 0; i < CELL_COLS; i++) {
-      for (var j = 0; j < CELL_ROWS; j++) {
-        var cell = this.cells.create(i * CELL_WIDTH, j * CELL_HEIGHT, 'cell')
-        cell.frame = 0
-        cell.inputEnabled = true
-        cell.events.onInputDown.add(this.addPlayerMarker, this)
-        this.game.physics.arcade.enable(cell)
-      }
-    }
-
     this.game.physics.startSystem(Phaser.Physics.ARCADE)
-
-    // this.sprite.events.onInputDown.add(this.clickListener, this)
+    this.logo = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'phaser')
+    this.logo.anchor.setTo(0.5, 0.5)
+    this.up_key = this.game.input.keyboard.addKey(Phaser.Keyboard.DOWN)
+    this.down_key = this.game.input.keyboard.addKey(Phaser.Keyboard.UP)
+    this.left_key = this.game.input.keyboard.addKey(Phaser.Keyboard.LEFT)
+    this.right_key = this.game.input.keyboard.addKey(Phaser.Keyboard.RIGHT)
+    this.space_key = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR)
+    this.emitter = this.createEmitter(this.logo, this.game)
   }
 
   checkGameComplete() {
-    let plays_left = this.cells.filter(function(i) { return i.frame === 0 }).list
-    console.log(plays_left)
-    if (plays_left.length === 0) {
-      this.game.state.start('gameover')
-    }
   }
 
-	update() {
-	}
+  zoomOut() {
+    let x = this.logo.scale.x
+    let y = this.logo.scale.y
+    this.logo.scale.setTo(x * 0.95, y * 0.95)
+  }
 
-  addPlayerMarker(sprite, pointer) {
-    console.log(this.cells)
-    if(sprite.frame === 0) {
-      sprite.frame = this.player
-      this.player = this.player === 1 ? 2 : 1
-    }
-    this.checkGameComplete()
+  zoomIn() {
+    let x = this.logo.scale.x
+    let y = this.logo.scale.y
+    this.logo.scale.setTo(x * 1.05, y * 1.05)
+  }
+
+  rotateLeft() {
+    this.logo.rotation -= 0.01
+  }
+
+  rotateRight() {
+    this.logo.rotation += 0.1
+  }
+
+  createEmitter(sprite, game) {
+    //let emitter = game.add.emitter(sprite.x - 40 , sprite.y - 140, 1)
+    let emitter = game.add.emitter(180 , 50, 1000)
+    emitter.makeParticles('phoenix')
+    // emitter.minParticleSpeed.set(-4, 4)
+    // emitter.maxParticleSpeed.set(4, 4)
+    emitter.setAlpha(0.3, 1)
+    // emitter.setScale(0.1, 0.1, 0.1, 0.1, 0, Phaser.Easing.Quadratic.Out, true)
+    // emitter.lifespan = 1500
+    // emitter.gravity = -50
+    // emitter.minRotation = 180
+    // emitter.maxRotation = 180
+    emitter.minParticleScale = 0.01
+    emitter.maxParticleScale = 0.09
+    // Attach the emitter to the sprite
+    emitter.minParticleSpeed.setTo(-300, 300)
+    emitter.maxParticleSpeed.setTo(300, 300)
+    // emitter.bounce.setTo(0.5, 0.5)
+    // emitter.angularDrag = -180;
+
+    emitter.rotation = -90
+
+    sprite.addChild(emitter)
+    emitter.start(false, 1000, 100)
+    return emitter
+  }
+
+  update() {
+    // this.rotateLeft()
+
+    // this.emitter.rotation = -this.logo.rotation
+
+    //this.emitter.rotate(this.logo.x, this.logo.y, 1, true)
+
+    this.game.physics.arcade.collide(this.emitter)
+    if (this.down_key.isDown) { this.zoomIn() }
+    if (this.up_key.isDown) { this.zoomOut() }
+    if (this.left_key.isDown) { this.rotateLeft() }
+    if (this.right_key.isDown) { this.rotateRight() }
+    if (this.space_key.isDown) { this.emitter.emitParticle() }
   }
 }
