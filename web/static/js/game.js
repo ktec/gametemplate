5c41d3a7f8e14e9e2ee190749ac3d50fc82241e9
Keith Salisbury <keith.salisbury@alliants.com>
2015-11-06 19:07:18 +0000

    wip

---
+++ web/static/js/game.js
@@ -0,0 +1,32 @@
+import {BootState} from "./states/boot"
+import {GameOverState} from "./states/gameover"
+import {MenuState} from "./states/menu"
+import {PlayState} from "./states/play"
+import {PreloadState} from "./states/preload"
+
+export class Game extends Phaser.Game {
+
+  // Initialize Phaser
+  constructor(width, height, container) {
+    super(width, height, Phaser.AUTO, container, null)
+
+    // Game States
+    this.state.add('boot', new BootState(), false)
+    this.state.add('gameover', new GameOverState(), false)
+    this.state.add('menu', new MenuState(), false)
+    this.state.add('play', new PlayState(), false)
+    this.state.add('preload', new PreloadState(), false)
+
+    this.score = 0
+    this.highscore = 0
+
+    // Start the "boot" state
+    this.state.start('boot')
+  }
+
+  newScore() {
+    if (this.score > this.highscore) {
+      this.highscore = this.score
+    }
+  }
+}
