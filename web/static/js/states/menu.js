cb8c8799b472e8c6d01a55759cb251fc4229cb0f
Keith Salisbury <keith.salisbury@alliants.com>
2015-11-15 11:24:23 +0000

    wip

--- web/static/js/states/menu.js
+++ web/static/js/states/menu.js
@@ -1,15 +1,30 @@
 export class MenuState extends Phaser.State {
+  preload() {
+    // this.load.script('filter', 'https://cdn.rawgit.com/photonstorm/phaser/master/filters/Fire.js');
+
+  }
+
   create() {
-    const label = this.addText('Hello World')
+    const label = this.addText("Hello World")
     label.anchor.setTo(0.5)
     label.inputEnabled = true
     label.input.enableDrag()
     label.events.onDragStart.add(this.dragStart, this)
     label.events.onDragStop.add(this.dragStop, this)
     this.label = label
+
+    let background = this.game.add.sprite(0, 0)
+  	background.width = 800
+  	background.height = 600
+
+  	this.filter = this.game.add.filter('Fire', 800, 600)
+  	this.filter.alpha = 0.0
+
+  	background.filters = [this.filter]
+
   }
 
-  addText(message, style = { font: '65px Arial', fill: '#ffffff' }) {
+  addText(message, style = { font: "65px Arial", fill: '#ffffff' }) {
     return this.add.text(this.world.centerX, this.world.centerY, message, style)
   }
 
@@ -30,5 +45,7 @@ export class MenuState extends Phaser.State {
     if (y) { this.label.y = y }
   }
 
-	update() {}
+	update() {
+    this.filter.update()
+  }
 }
