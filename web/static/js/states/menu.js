0422efe07403285aee13a0d532a440fd78a265f9
Keith Salisbury <keith.salisbury@alliants.com>
2015-11-20 21:51:53 +0000

    wip

--- web/static/js/states/menu.js
+++ web/static/js/states/menu.js
@@ -1,21 +1,27 @@
 export class MenuState extends Phaser.State {
   create() {
-    const label = this.addText('Hello World')
+    this.keyboard = new Keyboard()
+    // const label = this.addText('Hello World')
     // label.events.onDragStart.add(this.dragStart, this)
     // label.events.onDragStop.add(this.dragStop, this)
     // this.label = label
-    //
-    this.input.keyboard.addCallbacks(this, this.onDown)
+
+    this.input.keyboard.addCallbacks(this, this.onPress)
   }
 
-  onDown(e) {
-    this.addLetter(e.which)
+  onPress(event) {
+    // console.log(event)
+    // console.log(event.which)
+    let char = this.keyboard.charFromCode(event.which, event.shiftKey)
+    this.addLetter(char)
   }
 
-  addLetter(keyCode) {
-    let sprite = this.addText(String.fromCharCode(keyCode))
+  addLetter(char) {
+    //this._keys[keycode] = new Phaser.Key(this.game, keycode);
+    //console.log(Phaser.Keyboard[keyCode])
+    let sprite = this.addText(char)
     let tween = this.add.tween(sprite)
-    tween.to({ alpha: 0, x: Math.random() * 500, y: 2 * keyCode }, 800, Phaser.Easing.Linear.None)
+    tween.to({ alpha: 0, x: Math.random() * 500, y: 2 * char.charCodeAt(0) }, 800, Phaser.Easing.Linear.None)
     tween.onComplete.add(function () {
       sprite.destroy()
     })
@@ -48,4 +54,24 @@ export class MenuState extends Phaser.State {
   // }
   //
 	update() {}
+}
+
+class Keyboard {
+  constructor() {
+    this.data = {
+      81: ['q', 'Q'],
+      219: ['{','['],
+      221: ['}',']']
+    }
+  }
+  charFromCode(code, shiftKey) {
+    let char = ''
+    if (code in this.data) {
+      char = (shiftKey) ? this.data[code][1] : this.data[code][0]
+    } else {
+      char = String.fromCharCode(code)
+      console.log(code)
+    }
+    return char
+  }
 }
